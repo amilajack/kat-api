0dbf6d8b83da6c0bf506d8e714078467a5099792
Amila Welihinda <amilajack@gmail.com>
2016-07-29 09:52:05 -0700

    Changed endpoint

--- src/Torrent.js
+++ src/Torrent.js
@@ -2,7 +2,7 @@ import fetch from 'isomorphic-fetch';
 import cheerio from 'cheerio';
 
 
-export function search(query, endpoint = 'https://kickass.skillproxy.org') {
+export function search(query, endpoint = 'https://kat.al') {
   return fetch(`${endpoint}/usearch/${encodeURIComponent(query)}/?field=seeders&sorder=desc`, {
     mode: 'no-cors'
   })
